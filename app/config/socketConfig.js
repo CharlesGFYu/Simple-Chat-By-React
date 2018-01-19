04e1b8808548117dd2dc050118aa88e764742b0d
v_guifuyu(余贵福) <v_guifuyu@tencent.com>
2018-01-19 19:35:46 +0800

    commit files

---
+++ app/config/socketConfig.js
@@ -0,0 +1,10 @@
+export default {
+    development: {
+        HOST: 'localhost',
+        PORT: '3000'
+    },
+    production: {
+        HOST: 'mdzzapp.com',
+        PORT: '3000'
+    }
+}
---
+++ app/constants/activeList.js
@@ -0,0 +1,6 @@
+// roomList
+export const INIT_ROOM_LIST = 'ROOM_INIT_LIST';
+export const ADD_ROOM_ITEM = 'ROOM_ADD_ITEM';
+export const REMOVE_ROOM_ITEM = 'REMOVE_ROOM_ITEM';
+export const PUSH_HISTORY = 'ROOM_PUSH_HISTORY';
+export const UNSHIFT
